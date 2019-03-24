8d4b1d21a637d1300f694435f5ab1ef43208f5e5
Gerardas Martynovas <gerardas@8devices.com>
2019-03-24 16:05:34 +0200

    added new packages for electron building

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const appName = 'mindfights-app2';
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    backgroundColor: '#ffffff'
+  });
+
+  win.loadURL(`file://${__dirname}/dist/` + appName + '/index.html');
+  win.webContents.openDevTools();
+
+  win.on('closed', function () {
+    win = null;
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function() {
+  if (win === null) {
+    createWindow()
+  }
+});
